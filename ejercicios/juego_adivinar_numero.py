fc579322044d61f21fc16c5e041a17151aed8ddf
JFelipeDR <j.felipe.d.r.10@gmail.com>
2022-10-20 19:39:14 -0500

    Revision 1.1

--- ejercicios/juego_adivinar_numero.py
+++ ejercicios/juego_adivinar_numero.py
@@ -1,21 +1,26 @@
 #Ejercicio11
 import random;
-seguir="s"
-print("juego para adivinar un numero")
-while seguir=="s":
-    numero_aleatorio=random.randint(0,100)
-    entrada=int(input("ADIVINE UN NUMERO entre 0 y 100 "))
-    while numero_aleatorio!=entrada:
-        if entrada < numero_aleatorio:
-            print("EL NÚMERO ES MAYOR")
-            entrada=int(input("ESCRIBA OTRO NUMERO "))
-        else:
-            print("EL NÚMERO ES MENOR")
-            entrada=int(input("ESCRIBA OTRO NUMERO "))
-    print("USTED ADIVINO EL NUMERO")
-    seguir=(input("DESEA SEGUIR ? s/N "))
-if seguir=="n":
-    print("El programa termino")
-else:
-    while seguir!="s" and seguir!="n":
-        seguir=(input("DESEA SEGUIR ? s/N "))
+
+def main():
+    seguir = "s"
+    print("juego para adivinar un numero")
+    while seguir == "s":
+        numero_aleatorio = random.randint(0, 100)
+        entrada = int(input("ADIVINE UN NUMERO entre 0 y 100 "))
+        while numero_aleatorio != entrada:
+            if entrada < numero_aleatorio:
+                print("EL NÚMERO ES MAYOR")
+                entrada = int(input("ESCRIBA OTRO NUMERO "))
+            else:
+                print("EL NÚMERO ES MENOR")
+                entrada = int(input("ESCRIBA OTRO NUMERO "))
+        print("USTED ADIVINO EL NUMERO")
+        seguir = (input("DESEA SEGUIR ? s/N "))
+    if seguir == "n":
+        print("Hasta la proxima 👋")
+    else:
+        while seguir != "s" and seguir != "n":
+            seguir = (input("DESEA SEGUIR ? s/N "))
+
+if __name__ == '__main__':
+    main()
